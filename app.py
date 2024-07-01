bc9e3a5621d626a78da322dd900a0855df46d104
oussematurki0 <oussematurki0@gmail.com>
2024-07-01 13:43:18 +0200

    updated

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from flask_bcrypt import Bcrypt
+from flask import Flask, jsonify, request
+from models.base_model import Base, engine
+from models.data_manager import get_session
+from models.user import User
+from models.review import Review
+from models.place import Place
+from models.country import Country
+from models.city import City
+from models.amenity import Amenity
+
+app = Flask(__name__)
---
+++ data_manager.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
