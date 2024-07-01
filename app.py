106d25cb2af4a6051a46b0aa6eda909586c47098
ghofranaaa <smailikhaled841@gmail.com>
2024-07-01 13:45:35 +0100

    fixed

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 from models.base_model import BaseModel
-from data_manager import get_session
 from models.user import User
 from models.review import Review
 from models.place import Place
@@ -10,4 +9,3 @@ from models.amenity import Amenity
 
 app = Flask(__name__)
 app.secret_key = "secret key"
-db = SQLAlchemy(app)
