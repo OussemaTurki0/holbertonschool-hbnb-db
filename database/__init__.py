45b024f1d9877f183d1a9821d90e0e18ce4fc23c
oussematurki0 <oussematurki0@gmail.com>
2024-07-04 19:50:44 +0200

    updated

--- database/__init__.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
-
-DATA_BASE = "sqlite:///./data.db"
-
-# Create the SQLAlchemy engine and session
-engine = create_engine(DATA_BASE)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
