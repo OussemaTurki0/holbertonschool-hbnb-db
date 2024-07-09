45a757f68133daff26b99b7d42330781757bbff2
oussematurki0 <oussematurki0@gmail.com>
2024-07-09 02:42:42 +0200

    updated

--- migrations/env.py
+++ migrations/env.py
@@ -1,4 +1,3 @@
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from alembic import context
@@ -6,24 +5,14 @@ import os
 import sys
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
-from models import city, base_model  # Import all models here
-
-# Import app and config
+from models import city, base_model, user, review, place, country, city, amenity
 from api import create_app
 from config import DevelopmentConfig
 
-# Initialize your app with the development configuration
 app = create_app(config_class=DevelopmentConfig)
-
-# Get the SQLAlchemy extension from the app
 db = app.extensions['sqlalchemy'].db
-
-# This is the Alembic Config object, which provides access to the values
-# within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
